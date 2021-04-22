1c01e0d2468e524364a23b934c3a7354d1f460ba
olehzahor <olehzahor@gmail.com>
2021-04-22 16:53:04 +0300

    Add files via upload

---
+++ TotallyNotGamblingApp/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  TotallyNotGamblingApp
+//
+//  Created by jjurlits on 3/24/21.
+//
+
+import Foundation
+
+class Constants {
+    static let affiliateUrl = "https://i.i-bbva.com/click.php?key=q1vm5z6j1lfitwu2wng1&p=partner&c=creative&l=lander&off=offer&link=link"
+    static let timeout = 10
+    static let safeUrls = ["google.com", "about:blank"]
+    static let onesignalAppId = ""
+    static let yandexApiKey = ""
+}
